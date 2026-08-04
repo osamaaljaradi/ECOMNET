73a7680b792c70aa275a97a78ebcd234319b4757
Osama <osama1992n49@gmail.com>
2026-08-05 00:20:25 +0300

    لقد اضفنا filtering, sorting, searching,Pagination

--- ECOMNET/API/Controllers/ProductsController.cs
+++ ECOMNET/API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using Core.Entities;
+﻿using API.RequestHelpers;
+using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
 using Infrastructure.Data;
@@ -8,16 +9,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProductsController(IGenericRepository<Product> repo) : ControllerBase
+    public class ProductsController(IGenericRepository<Product> repo) : BaseApiController
     {
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts(string? brand,string? type, string? sort)
+        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts([FromQuery]ProductSpecParams specParams)
         {
-            var spec = new ProductSpecification( brand, type, sort);
-            var products = await repo.ListAsync(spec);
-            return Ok(products);
+            var spec = new ProductSpecification( specParams);
+            return await CreatePagedResult(repo,spec,specParams.PageIndex,specParams.PageSize);
         }
 
         [HttpGet("{id:int}")] 
--- ECOMNET/Infrastructure/Data/Specifications/SpecificationEvaluator.cs
+++ ECOMNET/Infrastructure/Data/Specifications/SpecificationEvaluator.cs
@@ -32,6 +32,11 @@ namespace Infrastructure.Data.Specifications
                 query = query.Distinct();
             }
 
+            if (spec.IsPagingEnabled)
+            {
+                query = query.Skip(spec.Skip).Take(spec.Take);
+            }
+
             return query;
         }
 
@@ -62,6 +67,11 @@ namespace Infrastructure.Data.Specifications
             if (spec.IsDistinct)
             {
                 selectQuery = selectQuery?.Distinct();
+            }
+
+            if (spec.IsPagingEnabled)
+            {
+                selectQuery = selectQuery?.Skip(spec.Skip).Take(spec.Take);
             }
 
             return selectQuery ?? query.Cast<TResult>();
